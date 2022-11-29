851d6f315b0de86236e94ea457e8f294c7bffca5
lehakot-create <lehakot85@inbox.ru>
2022-11-29 20:43:24 +0400

    добавлены всплывающие окна

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import Index
-
+from .views import Index, MachineDetail
 
 urlpatterns = [
     path('', Index.as_view()),
+    path('machine/<int:pk>/', MachineDetail.as_view(), name='machine_detail'),
 ]
--- Backend/views.py
+++ Backend/views.py
@@ -1,5 +1,5 @@
 from allauth.account.views import LoginView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .filters import MachineFilter, AuthMachineFilter, AuthMaintenanceFilter, AuthClaimFilter
 from .models import Machine, Maintenance, Claims
@@ -31,3 +31,15 @@ class Index(ListView):
 
 class MyLogin(LoginView):
     template_name = 'account/login.html'
+
+
+class MachineDetail(DetailView):
+    model = Machine
+    template_name = 'machine_detail.html'
+    context_object_name = 'machine'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['machine_maintenance'] = Maintenance.objects.filter(machine=self.kwargs.get('pk'))
+        context['machine_claim'] = Claims.objects.filter(machine=self.kwargs.get('pk'))
+        return context
